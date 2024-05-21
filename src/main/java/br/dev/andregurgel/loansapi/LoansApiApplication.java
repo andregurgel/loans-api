81ca5472ae8497222261003048dbb80d10b07ce1
Andre Gurgel <andregurgeldev@gmail.com>
2024-05-21 08:46:22 -0300

    feat: Initial commit.

---
+++ src/main/java/br/dev/andregurgel/loansapi/LoansApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.andregurgel.loansapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/andregurgel/loansapi/LoansApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.andregurgel.loansapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
